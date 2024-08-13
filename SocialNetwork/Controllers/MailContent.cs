72cd091498eb90da191468e80acb5b09ceb1c18c
thang109 <thangntde170747@fpt.edu.vn>
2024-08-13 11:11:09 +0700

    Social Project

--- SocialNetwork/Controllers/MailContent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SocialNetwork.Controllers
-{
-    internal class MailContent
-    {
-        public string To { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
-    }
-}
